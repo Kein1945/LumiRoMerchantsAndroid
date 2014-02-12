2b38a6241ae28d147dc3de7c56a4e55dbfc3d01c
Kein <kein.1945@gmail.com>
2014-02-12 17:50:25 +0400

    Foreground update cycle

--- src/com/lumiro/merchantmonitor/MainActivity.java
+++ src/com/lumiro/merchantmonitor/MainActivity.java
@@ -1,9 +1,9 @@
 package com.lumiro.merchantmonitor;
 
-import android.app.Activity;
-import android.app.AlertDialog;
+import android.app.*;
 import android.content.*;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.View;
@@ -34,6 +34,7 @@ public class MainActivity extends Activity {
             }
             if(intent.getAction().equals(SHOW_TOAST)){
                 Log.d(TAG, intent.getStringExtra("toast"));
+                createInfoNotification(intent.getStringExtra("toast"));
                 Toast.makeText(context, intent.getStringExtra("toast"), Toast.LENGTH_SHORT).show();
             }
         }
@@ -96,6 +97,10 @@ public class MainActivity extends Activity {
         intentFilter.addAction(UPDATE_MERC_LIST);
         intentFilter.addAction(SHOW_TOAST);
         bManager.registerReceiver(bReceiver, intentFilter);
+
+        Intent i = new Intent(this, Market_Service.class);
+        i.setAction(Market_Service.ACTION_SYNC_START);
+        startService(i);
     }
 
     public void addMerc(View view){
@@ -121,6 +126,7 @@ public class MainActivity extends Activity {
     }
 
     public void sync_merc_list_with_db(){
+        Toast.makeText(this, "Sync list", Toast.LENGTH_SHORT).show();
         List<Merc> mercs = db.getMercs();
         ArrayAdapter<Merc> adapter = (ArrayAdapter<Merc>) mercListView.getAdapter();
 
@@ -128,5 +134,35 @@ public class MainActivity extends Activity {
         for(Merc merc: mercs){
             adapter.add(merc);
         }
+    }
+
+    public void createInfoNotification(String message){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("LumiRo")
+                        .setContentText(message);
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(1, mBuilder.build());
     }
 }
--- src/com/lumiro/merchantmonitor/Market_Service.java
+++ src/com/lumiro/merchantmonitor/Market_Service.java
@@ -1,13 +1,11 @@
 package com.lumiro.merchantmonitor;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.Service;
+import android.app.*;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.widget.Toast;
 import com.lumiro.merchantmonitor.Market.Parser;
@@ -15,6 +13,7 @@ import com.lumiro.merchantmonitor.db.DBAdaptor;
 import com.lumiro.merchantmonitor.db.ItemAdapter;
 import com.lumiro.merchantmonitor.helper.MerchantUpdater;
 
+import android.text.format.Time;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,8 +25,11 @@ public class Market_Service extends Service implements Runnable {
 
     public List<String> merchants = new ArrayList<String>();
 
+    private boolean looped = false;
+
     public static final String ACTION_SYNC_MERC = "com.lumiro.merchantmonitor.SYNC_MERC";
     public static final String ACTION_SYNC_MERCS = "com.lumiro.merchantmonitor.SYNC_MERCS";
+    public static final String ACTION_SYNC_START = "com.lumiro.merchantmonitor.SYNC_START";
 
     public IBinder onBind(Intent intent) {
         return null;
@@ -36,21 +38,28 @@ public class Market_Service extends Service implements Runnable {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.e(TAG, "Start service");
     }
 
     public int onStartCommand (Intent intent, int flags, int startId){
+        Log.e(TAG, intent.getAction());
         if (ACTION_SYNC_MERC.equals(intent.getAction())) {
+            Log.e(TAG, "Start merc update");
             merchants.add( intent.getStringExtra("merc_name") );
 
             Thread t = new Thread(this);
             t.start();
         }
         if (ACTION_SYNC_MERCS.equals(intent.getAction())) {
-            DBAdaptor db = new DBAdaptor(this);
-            List<Merc> mersc = db.getMercs();
-            for(Merc merc : mersc){
-                merchants.add( merc.getName() );
-            }
+            Log.e(TAG, "Start update");
+            merchants = new ArrayList<String>();
+            Thread t = new Thread(this);
+            t.start();
+        }
+        if (ACTION_SYNC_START.equals(intent.getAction())) {
+            Log.e(TAG, "Start loop");
+            merchants = new ArrayList<String>();
+            looped = true;
             Thread t = new Thread(this);
             t.start();
         }
@@ -59,6 +68,28 @@ public class Market_Service extends Service implements Runnable {
 
     @Override
     public void run() {
+        if(looped){
+            Log.e(TAG, "Add event loop");
+            Intent i = new Intent(this, Market_Service.class);
+            i.setAction(Market_Service.ACTION_SYNC_START);
+
+            PendingIntent pendingIntent = PendingIntent.getService(this, 0, i, 0);
+
+            AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+            Time time = new Time();
+            time.set(System.currentTimeMillis() + UPDATE_INTERVAL);
+
+            long nextUpdate = time.toMillis(false);
+            alarmManager.set(AlarmManager.RTC_WAKEUP, nextUpdate, pendingIntent);
+        }
+        if(0 == merchants.size()){
+            DBAdaptor db = new DBAdaptor(this);
+            List<Merc> mersc = db.getMercs();
+            for(Merc merc : mersc){
+                merchants.add( merc.getName() );
+            }
+        }
+        Log.d(TAG, "Service start sync");
         for(String merchant_name : merchants){
             Log.d(TAG, "Requested SYNC_MERC action ["+merchant_name+"]");
             DBAdaptor db = new DBAdaptor(this);
@@ -69,16 +100,14 @@ public class Market_Service extends Service implements Runnable {
             updater.updateItems(items);
 
             if(updater.isNew()){
-                Log.d(TAG, "Merchant is new");
                 Intent RTReturn = new Intent(MainActivity.SHOW_TOAST);
                 RTReturn.putExtra("toast", "Merchant "+merc.getName()+" is new ");
                 LocalBroadcastManager.getInstance(this).sendBroadcast(RTReturn);
             }
             if(updater.isOffline()){
                 Intent RTReturn = new Intent(MainActivity.SHOW_TOAST);
-                RTReturn.putExtra("toast", "Merchant "+merc.getName()+" is offline");
+                RTReturn.putExtra("toast", "Merchant " + merc.getName() + " is offline");
                 LocalBroadcastManager.getInstance(this).sendBroadcast(RTReturn);
-                Log.d(TAG, "Merchant is offline");
             }
 
             db.updateMerc(merc);
@@ -87,4 +116,5 @@ public class Market_Service extends Service implements Runnable {
         Intent RTReturn = new Intent(MainActivity.UPDATE_MERC_LIST);
         LocalBroadcastManager.getInstance(this).sendBroadcast(RTReturn);
     }
+    private static final long UPDATE_INTERVAL = 20 * DateUtils.MINUTE_IN_MILLIS;
 }
